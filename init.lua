f2994743f240345ef2e292f55f422cd7e937f61c
esquire-out <148168035+esquire-out@users.noreply.github.com>
2023-10-23 01:56:53 +0000

    fixed Telescope version mismatch, added eza

--- init.lua
+++ init.lua
@@ -65,7 +65,7 @@ require('packer').startup(function(use)
 	-- Telescope
 	use {
 		'nvim-telescope/telescope.nvim',
-		tag = '0.1.0',
+		tag = '0.1.4',
 		requires = {{ 'nvim-lua/plenary.nvim' }}
 	}
     
--- misc/utilities.sh
+++ misc/utilities.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cargo install --locked zellij
+cargo install eza
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
